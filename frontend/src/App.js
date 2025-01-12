f485764758f3743e017bb3bc7e379be2b209d8a3
Rene <rene@devhousestudio.com>
2025-01-12 18:43:43 -0500

    initial project set up

---
+++ frontend/src/App.js
@@ -0,0 +1,14 @@
+import logo from './images/ChronoCanvasLogo.png';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <img src={logo} alt="logo" style={{width: '150px'}} />
+      </header>
+    </div>
+  );
+}
+
+export default App;
