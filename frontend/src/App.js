66dc387cd43ad33fcee7c582a826bb249772445e
Rene <rene@devhousestudio.com>
2025-01-12 19:38:41 -0500

    added bootstrap sass

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,5 +1,5 @@
 import logo from './images/ChronoCanvasLogo1.png';
-import './App.css';
+import './styles/App.css';
 
 function App() {
   return (
--- frontend/src/index.js
+++ frontend/src/index.js
@@ -1,8 +1,9 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import './index.css';
+import './styles/index.css';
 import App from './App';
 import 'bootstrap/dist/css/bootstrap.min.css';
+import './styles/custom-bootstrap.scss';
 
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
