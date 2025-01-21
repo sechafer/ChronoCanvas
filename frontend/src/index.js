6391c161ac0311fcacf291b6087c3d89cdb578d2
Samuel Chacon <sechafer@hotmail.com>
2025-01-21 00:29:43 -0300

    Update index.js

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -5,7 +5,13 @@ import App from './App';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import './styles/custom-bootstrap.scss';
 import 'react-datepicker/dist/react-datepicker.css';
+const express = require('express'); // Si usas Express
+const app = express();
 
+const port = process.env.PORT || 3000; // Obtiene el puerto del entorno o usa 3000 como fallback
+app.listen(port, () => {
+  console.log(`Server running on port ${port}`);
+});
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
