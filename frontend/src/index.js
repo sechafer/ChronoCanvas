efdcfc99108ddbef3bce7296590bb779f4a15630
Samuel Chacon <sechafer@hotmail.com>
2025-01-21 00:11:31 -0300

    Update index.js

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -6,11 +6,6 @@ import 'bootstrap/dist/css/bootstrap.min.css';
 import './styles/custom-bootstrap.scss';
 import 'react-datepicker/dist/react-datepicker.css';
 
-const PORT = process.env.PORT || 3000;
-app.listen(PORT, () => {
-  console.log(`Server is running on port ${PORT}`);
-});
-
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
