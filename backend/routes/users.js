dfe124c6f7f8bcf95d6a12089f47ecadcfdbcda6
Samuel Chacon <sechafer@gmail.com>
2025-02-10 19:02:39 -0300

    eliminacion de todo tipo de autorizacion apra user get all

--- backend/routes/users.js
+++ backend/routes/users.js
@@ -5,7 +5,7 @@ const validation = require('../middleware/validate.js');
 const { verifyToken, isAuthenticated } = require('../middleware/auth.js'); // Importamos el middleware de JWT
 
 router.get('/:id', isAuthenticated,verifyToken, validation.checkMongoId, usersController.getSingle);
-router.get('/', isAuthenticated, usersController.getAll);
+router.get('/', usersController.getAll);
 router.post('/', isAuthenticated,verifyToken, validation.saveUser, usersController.createUser);
 router.put('/:id', isAuthenticated,verifyToken, validation.checkMongoId, validation.saveUser, usersController.updateUser);
 router.delete('/:id', isAuthenticated,verifyToken, validation.checkMongoId, usersController.deleteUser);
